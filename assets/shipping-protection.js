606e7ba7aea4e5918bf58e7af66ab2961d6c60ee
gloross <hello@gloross.com>
2022-07-13 13:41:00 +0300

    add progress to shipping protection

---
+++ assets/shipping-protection.js
@@ -0,0 +1,86 @@
+const selectors = {
+  sectionShippingProtection: ".js-section-shipping-protection",
+  productCheckbox: ".js-product-checkbox",
+  shippingProtectionPrice: ".js-shipping-protection-price",
+};
+
+const section = document.querySelector(selectors.sectionShippingProtection);
+const productHandle = section.dataset.productHandle;
+const productCheckbox = document.querySelector(selectors.productCheckbox);
+
+function addItemToCart(productId) {
+  let formData = {
+    items: [
+      {
+        id: productId,
+        quantity: 1,
+      },
+    ],
+  };
+  fetch("/cart/add.js", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      window.location.reload();
+    })
+    .then((data) => {
+      console.log("item added", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function removeItemFromCart(productId) {
+  let productIdJSON = JSON.parse('{"' + productId + '": 0}');
+  let updateData = {
+    updates: productIdJSON,
+  };
+  fetch("/cart/update.js", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updateData),
+  })
+    .then(function (data) {
+      console.log(data.status, data.body);
+      if (data.status == 200) {
+        console.log("item being removed to cart");
+        window.location.reload();
+      } else {
+        console.error("Request returned an error", data);
+      }
+    })
+    .catch(function (error) {
+      console.error("Request failed", error);
+    });
+}
+
+function checkboxChange(element, data) {
+  element.addEventListener("change", (e) => {
+    if (element.checked) {
+      addItemToCart(data.variants[0].id);
+    } else {
+      removeItemFromCart(data.variants[0].id);
+    }
+  });
+}
+
+function fetchProduct(productHandle) {
+  let productUrl = "/products/" + productHandle + ".js";
+  fetch(productUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      checkboxChange(productCheckbox, data);
+      document.querySelector(selectors.shippingProtectionPrice).innerHTML = `${Shopify.formatMoney(data.price)}`;
+    });
+}
+
+fetchProduct(productHandle);
